d68dd50cb768d5483ec357026ddd9cb0b05cf900
julienty <julien_paulus@live.be>
2018-05-04 22:02:39 +0200

    factorial

---
+++ factorial.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+  int factorial;
+  double result;
+  int init;
+
+  printf("Voulez-vous calculer la factorielle de quel nombre ?");
+  scanf("%d", &factorial);
+
+  init = factorial;
+  result = factorial;
+
+  while (factorial != 1)
+  {
+    factorial--;
+    result = result * factorial;
+  }
+  printf("La factorielle de %d est %f",init,result);
+}
